233d2d28743fa8c8a227bf434b6cbbe0b711a402
Said Kadrioski <said@kadrioski.de>
2021-12-17 03:42:20 +0100

    Finished Day 8 Part 2. Mapped

--- src/bin/8.rs
+++ src/bin/8.rs
@@ -1,4 +1,4 @@
-use std::{str::{FromStr, ParseBoolError}};
+use std::{str::{FromStr, ParseBoolError}, collections::BTreeMap};
 use asos::reader::read_lines;
 
 fn part2(lines: &Vec<Shiffre>) -> usize {
@@ -7,11 +7,11 @@ fn part2(lines: &Vec<Shiffre>) -> usize {
         let mut number = "".to_string();
         let mapper = line.map_signal();
         for s in &line.searcher {
-            for (i, m) in mapper.iter().enumerate() {
-                if s.len() == m.len() && s.chars().all(|c| m.contains(c)) {
-                    number += &i.to_string();
-                }
-            }
+            let k = mapper.keys()
+                .find(|t| s.len() == t.len() && s.chars().all(|c| t.contains(c)))
+                .unwrap()
+                .to_owned();
+            number = number + &mapper.get(k).unwrap().to_string();
         }
         let n: usize = number.parse().unwrap();
         sum += n;
@@ -52,51 +52,54 @@ impl FromStr for Shiffre {
 }
 
 impl Shiffre {
-fn map_signal(&self) -> [&str;10]  {
-    let one = self.resolver
-        .iter()
-        .find(|&s| s.len() == 2)
-        .unwrap();
-    let four = self.resolver
-        .iter()
-        .find(|&s| s.len() == 4)
-        .unwrap();
-    let seven = self.resolver
-        .iter()
-        .find(|&s| s.len() == 3)
-        .unwrap();
-    let eight = self.resolver
-        .iter()
-        .find(|&s| s.len() == 7)
-        .unwrap();
-
-    let three = self.resolver
-        .iter()
-        .find(|&s| s.len() == 5 && one.chars().all(|c| s.contains(c)))
-        .unwrap();
-    let nine = self.resolver
-        .iter()
-        .find(|&s| s.len() == 6 && four.chars().all(|c| s.contains(c)))
-        .unwrap();
-    let six = self.resolver
-        .iter()
-        .find(|&s| s.len() == 6 && !one.chars().all(|c| s.contains(c)))
-        .unwrap();
-    let zero = self.resolver
-        .iter()
-        .find(|s| s.len() == 6 && s != &nine && s != &six)
-        .unwrap();
-
-    let five = self.resolver
-        .iter()
-        .find(|s| s.len() == 5 && s != &three && s.chars().all(|c| six.contains(c)))
-        .unwrap();
-    let two = self.resolver
-        .iter()
-        .find(|s| s.len() == 5 && s != &five && s != &three)
-        .unwrap();
-    [zero, one, two, three, four, five, six, seven, eight, nine]
-}
+    fn map_signal(&self) -> BTreeMap<&String, usize>  {
+        let mut map: BTreeMap<&String, usize> = BTreeMap::new();
+        let one = self.resolver
+            .iter()
+            .find(|&s| s.len() == 2)
+            .unwrap();
+        let four = self.resolver
+            .iter()
+            .find(|&s| s.len() == 4)
+            .unwrap();
+        let seven = self.resolver
+            .iter()
+            .find(|&s| s.len() == 3)
+            .unwrap();
+        let eight = self.resolver
+            .iter()
+            .find(|&s| s.len() == 7)
+            .unwrap();
+        let three = self.resolver
+            .iter()
+            .find(|&s| s.len() == 5 && one.chars().all(|c| s.contains(c)))
+            .unwrap();
+        let nine = self.resolver
+            .iter()
+            .find(|&s| s.len() == 6 && four.chars().all(|c| s.contains(c)))
+            .unwrap();
+        let six = self.resolver
+            .iter()
+            .find(|&s| s.len() == 6 && !one.chars().all(|c| s.contains(c)))
+            .unwrap();
+        let zero = self.resolver
+            .iter()
+            .find(|s| s.len() == 6 && s != &nine && s != &six)
+            .unwrap();
+        let five = self.resolver
+            .iter()
+            .find(|s| s.len() == 5 && s != &three && s.chars().all(|c| six.contains(c)))
+            .unwrap();
+        let two = self.resolver
+            .iter()
+            .find(|s| s.len() == 5 && s != &five && s != &three)
+            .unwrap();
+        let a: [&String; 10] = [zero, one, two, three, four, five, six, seven, eight, nine];
+        for (i, t) in a.iter().enumerate() {
+            map.insert(t, i);
+        }
+        map
+    }
 }
 
 struct Shiffre {
